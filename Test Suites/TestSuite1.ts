d01c768b4e397b851785b17d63c34053aa45f729
unknown <BCN-9BC71G2$@usersad.everis.int>
2018-08-23 10:45:28 +0200

    test1

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-08-23T10:45:12</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>0b4759cf-3eae-45e7-a2d1-b54744a6bcdf</testSuiteGuid>
+   <testCaseLink>
+      <guid>5aa36326-fc52-42ec-a1e6-ee415c3a440b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
